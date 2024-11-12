117eed16405a3400a968ea16e1cb6fc3a961e493
Mimo2097 <Massimo.Santostasi@student.uibk.ac.at>
2024-11-12 10:52:32 +0100

    included google maps and initialised it for luxembourd

--- screens/map.js
+++ screens/map.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const Map= ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Text>Map Screen</Text>
-      <Button title='Click Here!' alert={'Button Clicked!'}/>
+      <MapView style={StyleSheet.absoluteFill} provider={MapView.PROVIDER_GOOGLE}
+      initialRegion={{
+        latitude: 49.6116,
+        longitude: 6.1319,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      }}/>
     </View>
   );
 };
@@ -20,5 +25,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#8fcbbc'
+  },
+  map:{
+    width: '100%',
+    height: '100%'
   },
 });
